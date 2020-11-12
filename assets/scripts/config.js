118aae8f2f36f7f48e43d6c45f285de24a861e06
kdoxsey <kdoxse@gmail.com>
2020-11-12 16:15:09 -0500

    trying to deploy. index works, show and delete do not

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/powerful-chamber-37250.git',
+  production: 'https://powerful-chamber-37250.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
