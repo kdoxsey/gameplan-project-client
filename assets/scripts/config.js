2fa0cdc1d85eeb0f3c973c17b5783193c5fb006f
kdoxsey <kdoxse@gmail.com>
2020-11-12 13:07:55 -0500

    heroku url added

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://powerful-chamber-37250.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
