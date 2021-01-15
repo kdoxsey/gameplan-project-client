04b826c0a5602fa3971fd1434d5ecd166fd9c49c
kdoxsey <kdoxse@gmail.com>
2021-01-15 15:21:24 -0500

    removed / at the end of apiUrl

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://gentle-hollows-71842.herokuapp.com/',
+  production: 'https://gentle-hollows-71842.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
